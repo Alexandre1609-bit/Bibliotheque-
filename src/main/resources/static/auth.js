ce1a5c3a9b593f65deddcab1522d1306c798b0b9
Alexandre <alexandre.regnier59@live.fr>
2025-11-26 16:58:05 +0100

    Update de Login.html et ajout du JS auth.js.

---
+++ src/main/resources/static/auth.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const loginForm = document.getElementById('loginForm');
+    loginForm.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+
+    const emailTape = document.getElementById('email').value;
+    const passwordTape = document.getElementById('password').value;
+
+
+    const dataToSend = {
+        email: emailTape,
+        pswd: passwordTape
+    };
+
+    //Fetch
+    fetch('http://localhost:8080/login', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToSend)
+    })
+    .then(reponse => {
+
+            console.log("Réponse brute :", reponse);
+            return reponse.json();
+        })
+        .then(user => {
+
+            if (user) {
+                console.log("Bienvenue " + user.name);
+                alert("Connexion réussie !");
+            } else {
+                console.log("Erreur : Utilisateur non trouvé");
+                alert("Email ou mot de passe incorrect");
+            }
+        })
+        .catch(erreur => {
+            console.error("Problème technique :", erreur);
+        });
+
+        });
+
+    });
+
